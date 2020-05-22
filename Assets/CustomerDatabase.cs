32e9d10f4a4fe221ee8f1f2b685d98521f185e3e
Paul B <paul.belfrage@gmail.com>
2020-05-22 12:56:47 +0200

    Understand When and Why to Use Custom Classes
    Understand When and Why to Use Custom Classes

---
+++ Assets/CustomerDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDatabase : MonoBehaviour
+{
+
+    public Customer adam;
+    public Customer amy;
+    public Customer abby;
+    public Customer eric;
+
+    private void Start()
+    {
+        adam = new Customer("Adam", "Adamson", 26, "M", "Teacher");
+        amy = new Customer("Amy", "", 36, "F", "Engineer");
+        abby = new Customer("Abby", "Lang", 56, "F", "Scientist");
+    }
+}
