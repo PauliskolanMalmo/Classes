213fe0afedf5ed5c3751b0cb1643e6ca10491db8
Paul B <paul.belfrage@gmail.com>
2020-05-25 13:47:31 +0200

    RPG Spell System Presentation part 1
    RPG Spell System Presentation part 1

---
+++ Assets/Spell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public int levelRequired;
+    public int itemRequired;
+    public int expGained;
+
+    public Spell(string name, int levRequired, int itemRequired, int exp)
+    {
+        this.name = name;
+        this.levelRequired = levRequired;
+        this.itemRequired = itemRequired;
+        this.expGained = exp;
+    }
+
+    public void Cast()
+    {
+        Debug.Log("Cast Spell!");
+    }
+}
---
+++ Assets/Wizard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonoBehaviour
+{
+    public Spell fireBlast;
+
+    public int level = 1;
+    public int exp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fireBlast = new Spell("fireblast", 1, 27, 35);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /*
+            fireBlast.Cast();
+            exp += fireBlast.expGained;
+            */
+        }
+
+    }
+
+}
